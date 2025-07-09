250ae48271263c99f8d19a7c8d1c24999472ac92
giorgiameffe <giorgiameffe03@gmail.com>
2025-07-09 11:31:06 +0200

    feat: complete exercise one (modules)

---
+++ src/typescript-exercises/01-modules-import-export/exercises.ts
@@ -0,0 +1,9 @@
+// ✏️ Esercizio 1 – Named Export e Import
+// Importa le funzioni add e multiply da math.ts e usale per stampare:
+// 3 + 10 = 13
+// 4 * 5 = 20
+
+import { add, multiply } from './utils/math';
+
+console.log(add(3, 13));
+console.log(multiply(4, 5));
---
+++ src/typescript-exercises/01-modules-import-export/utils/math.ts
@@ -0,0 +1,9 @@
+// Esporta due funzioni: add (somma) e multiply (moltiplicazione)
+
+export function add(a: number, b: number): number {
+    return a + b;
+}
+
+export function multiply(a: number, b: number): number {
+    return a * b;
+}
