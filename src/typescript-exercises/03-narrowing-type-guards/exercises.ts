3b996b1f6c00cb47f41744305054790d8b401bc1
giorgiameffe <giorgiameffe03@gmail.com>
2025-07-10 12:27:41 +0200

    feat: complete first exercise ( personalized type-guards)

--- src/typescript-exercises/03-narrowing-type-guards/exercises.ts
+++ src/typescript-exercises/03-narrowing-type-guards/exercises.ts
@@ -1,4 +1,6 @@
-// ✏️ Esercizio 1 - Gestione stringa o numero (narrowing)
+// Narrowing - Type GUards integrate
+
+// ✏️ Esercizio 1 - Gestione stringa o numero
 // Scrivi una funzione chiamata handleInput che riceve un parametro input di tipo string o number.
 // Se input è una stringa, stampa in console la sua lunghezza.
 // Se input è un numero, stampa in console il numero arrotondato a due cifre decimali.
@@ -15,7 +17,7 @@ function handleInput(input: string | number): void {
 handleInput('Gelato');
 handleInput(30.6789);
 
-// ✏️ Esercizio 2 - Riconoscere tipi primitivi (narrowing)
+// ✏️ Esercizio 2 - Riconoscere tipi primitivi
 // Scrivi una funzione chiamata checkType che riceve un parametro value di tipo boolean, string o number.
 // Se value è un booleano, stampa "Valore booleano".
 // Se value è una stringa, stampa "Valore stringa".
@@ -36,7 +38,7 @@ checkType(true);
 checkType('Gatto');
 checkType(5);
 
-// ✏️ Esercizio 3 - Stringa singola o array di stringhe (narrowing)
+// ✏️ Esercizio 3 - Stringa singola o array di stringhe
 // Scrivi una funzione chiamata printStringOrArray che riceve un parametro x di tipo string
 // oppure array di stringhe (string[]).
 // Se x è una stringa, stampa "Singola stringa: " seguito dal valore di x.
@@ -54,7 +56,7 @@ function printStringOrArray(x: string | string[]): void {
 printStringOrArray('Cane');
 printStringOrArray(['Cocomero', 'Mango', 'Albicocca']);
 
-// ✏️ Esercizio 4 - Valore nullo o numero (narrowing)
+// ✏️ Esercizio 4 - Valore nullo o numero 
 // Scrivi una funzione chiamata displayValue che riceve un parametro input di tipo number | null.
 // Se input è null, stampa "Nessun valore".
 // Se è un numero, stampa "Valore: " seguito dal numero.
@@ -71,7 +73,7 @@ function displayValue(input: number | null): void {
 displayValue(null);
 displayValue(8);
 
-// ✏️ Esercizio 5 - Booleano o array di numeri (narrowing)
+// ✏️ Esercizio 5 - Booleano o array di numeri 
 // Scrivi una funzione chiamata processData che riceve un parametro data di tipo boolean | number[].
 // Se data è un booleano, stampa "Attivo" se è true, o "Inattivo" se è false.
 // Se è un array di numeri, stampa "Somma: " seguito dalla somma di tutti i numeri nell’array.
@@ -116,3 +118,28 @@ processData([3, 5, 5]);
 
 }
 
+// Narrowing - Type GUards personalizzate
+
+// ✏️ Esercizio 1 – isString
+// Scrivi una funzione isString che prende un valore di tipo unknown e ritorna value is string.
+// La funzione deve restituire true solo se il valore è una stringa.
+
+function isString(value: unknown): value is string {
+
+    return typeof value === 'string';
+}
+
+console.log(isString('Come stai?'));
+console.log(isString(5));
+
+// ✏️ Esercizio 2 – isNumberArray
+// Scrivi una funzione isNumberArray che prende un valore di tipo unknown e ritorna value is number[].
+// Deve restituire true solo se il valore è un array e tutti gli elementi sono numeri.
+
+
+
+
+// ✏️ Esercizio 3 – isPerson
+// Definisci il tipo Person con una proprietà name stringa.
+// Scrivi una funzione isPerson che prende un valore di tipo unknown e ritorna value is Person.
+// La funzione deve verificare che il valore sia un oggetto non nullo con una proprietà name di tipo stringa.
