6594133e9073b148f307d9be93eaa6ac21145e5d
giorgiameffe <giorgiameffe03@gmail.com>
2025-07-10 10:57:35 +0200

    feat: complete exercise two (narrowing)

--- src/typescript-exercises/03-narrowing-type-guards/exercises.ts
+++ src/typescript-exercises/03-narrowing-type-guards/exercises.ts
@@ -3,7 +3,7 @@
 // Se input è una stringa, stampa in console la sua lunghezza.
 // Se input è un numero, stampa in console il numero arrotondato a due cifre decimali.
 
-function handleInput(input: string | number) {
+function handleInput(input: string | number): void {
 
     if (typeof input === 'string') {
         console.log(input.length);
@@ -15,15 +15,26 @@ function handleInput(input: string | number) {
 handleInput('Gelato');
 handleInput(30.6789);
 
-
 // ✏️ Esercizio 2 - Riconoscere tipi primitivi (narrowing)
 // Scrivi una funzione chiamata checkType che riceve un parametro value di tipo boolean, string o number.
 // Se value è un booleano, stampa "Valore booleano".
 // Se value è una stringa, stampa "Valore stringa".
 // Se value è un numero, stampa "Valore numero".
 
+function checkType(value: boolean | string | number): void {
 
+    if (typeof value === 'boolean') {
+        console.log('Valore booleano')
+    } else if (typeof value === 'string') {
+        console.log('Valore stringa')
+    } else {
+        console.log('Valore numero')
+    }
+}
 
+checkType(true);
+checkType('Gatto');
+checkType(5);
 
 // ✏️ Esercizio 3 - Stringa singola o array di stringhe (narrowing)
 // Scrivi una funzione chiamata printStringOrArray che riceve un parametro x di tipo string
