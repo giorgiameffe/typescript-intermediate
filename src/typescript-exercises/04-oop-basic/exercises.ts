64fd50aa00a62cc4e246047f0d0fd897510ca891
giorgiameffe <giorgiameffe03@gmail.com>
2025-07-11 11:46:01 +0200

    feat: complete exercise three (oop)

--- src/typescript-exercises/04-oop-basic/exercises.ts
+++ src/typescript-exercises/04-oop-basic/exercises.ts
@@ -51,6 +51,31 @@
     personOne.sayHi();
 }
 
+// ✏️ Esercizio 3: Crea una classe Car
+// La classe si chiama Car e deve avere due proprietà:
+// brand (marca dell’auto, es. "Fiat")
+// year (anno di immatricolazione, es. 2020)
+// Aggiungi un metodo info() che stampa: "Questa auto è una {brand} del {year}"
+
+{
+    class Car {
+        brand: string;
+        year: number;
+
+        constructor(brand: string, year: number) {
+            this.brand = brand;
+            this.year = year
+        }
+
+        info(): void {
+            console.log(`Questa auto è una ${this.brand} del ${this.year}`);
+        }
+    }
+
+    const carOne = new Car('Mercedes', 2020);
+    carOne.info();
+}
+
 // ✏️ Esercizio 3: Crea una classe Animale con ereditarietà
 // Crea una classe Animale con proprietà: nome
 // Aggiungi un metodo faVerso() che stampa: "L'animale fa un verso generico".
