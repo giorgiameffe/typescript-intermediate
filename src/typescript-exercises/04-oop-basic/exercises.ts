3d73d821d4599e6eb4d9c80d10dea9b5d182b51d
giorgiameffe <giorgiameffe03@gmail.com>
2025-07-11 11:25:31 +0200

    feat: update package.json and complete exercise one (oop)

---
+++ src/typescript-exercises/04-oop-basic/exercises.ts
@@ -0,0 +1,43 @@
+// ✏️ Esercizio 1: Crea una classe Person
+// Crea una classe Persona e aggiungi le proprietà: name e age
+// Crea un metodo sayHi() che stampa: "Ciao, mi chiamo {name} e ho {age} anni".
+
+{
+    class Person {
+        name: string;
+        age: number;
+
+        constructor(name: string, age: number) {
+            this.name = name;
+            this.age = age;
+        }
+
+        sayHi() {
+            console.log(`Ciao, mi chiamo ${this.name} e ho ${this.age} anni`)
+        }
+    }
+
+    const personOne = new Person('Mario', 22);
+    personOne.sayHi();
+
+}
+
+// ✏️ Esercizio 2: Aggiungi un metodo per aggiornare l'età
+// Aggiungi alla classe Persona un metodo:
+// invecchia(anni: number) che aumenta l'età della persona.
+
+
+// ✏️ Esercizio 3: Crea una classe Animale con ereditarietà
+// Crea una classe Animale con proprietà: nome
+// Aggiungi un metodo faVerso() che stampa: "L'animale fa un verso generico".
+// Crea una sottoclasse Cane che estende Animale:
+// Override del metodo faVerso() per stampare: "Bau!"
+
+
+
+// ✏️ Esercizio 4 — Classe Libro con getter/setter
+// Crea una classe Libro con una proprietà titolo.
+// Includi un costruttore che inizializza il titolo.
+// Aggiungi due metodi:
+// getTitolo() che restituisce il titolo del libro.
+// setTitolo(nuovoTitolo: string) che aggiorna il titolo.
