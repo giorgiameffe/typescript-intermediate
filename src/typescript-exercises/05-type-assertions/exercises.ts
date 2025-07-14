a8a3cd71210e8ad46689cbaf1a23cfdda2ffe90b
giorgiameffe <giorgiameffe03@gmail.com>
2025-07-14 16:35:54 +0200

    feat: update pakeage.json and complete exercise one (type-assertions)

---
+++ src/typescript-exercises/05-type-assertions/exercises.ts
@@ -0,0 +1,28 @@
+// ✏️ Esercizio 1 — Type assertion su any stringa
+// Hai una variabile valore di tipo any che contiene una stringa.
+// Usa una type assertion per trattarla come string, poi stampa la lunghezza della stringa (.length).
+
+const value: any = 'Montagna';
+const valueLength = (value as string).length;
+console.log(valueLength);
+
+// ✏️ Esercizio 2 — Type assertion su any numero
+// Hai una variabile numero di tipo any che contiene un numero.
+// Usa una type assertion per trattarla come number e stampane il valore.
+
+
+// ✏️ Esercizio 3 — Type assertion su unknown stringa
+// Hai una variabile qualcosa di tipo unknown che contiene una stringa.
+// Usa una type assertion per trattarla come string e stampa la sua lunghezza.
+
+
+
+// ✏️ Esercizio 4 — Type assertion su elemento HTML
+// Hai un elemento HTML creato con document.createElement("input").
+// Usa una type assertion per trattarlo come HTMLInputElement e imposta il valore del campo input a "Mario".
+
+
+
+// ✏️ Esercizio 5 — Type assertion su oggetto
+// Hai un oggetto vuoto che stai "forzando" a essere del tipo { nome: string }.
+// Usa la type assertion per dichiarare il tipo, poi assegna un valore al campo nome e stampalo.
