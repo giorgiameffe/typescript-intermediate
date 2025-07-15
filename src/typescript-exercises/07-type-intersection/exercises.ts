9e30e40409d3cbce1dac2a111d802bafd14851fa
giorgiameffe <giorgiameffe03@gmail.com>
2025-07-15 12:48:49 +0200

    feat: complete exercise two (type-intersection)

--- src/typescript-exercises/07-type-intersection/exercises.ts
+++ src/typescript-exercises/07-type-intersection/exercises.ts
@@ -23,3 +23,43 @@
 
     console.log(personOne);
 }
+
+// ✏️ Esercizio 2 – Funzione che riceve un tipo intersezione
+// Crea due tipi:
+// LoginData con proprietà username e password
+// Profile con proprietà name e age
+// Crea un tipo UserAccount che sia l’intersezione tra LoginData e Profile.
+// Crea una funzione printAccount che riceve un oggetto che è l’intersezione dei due
+// La funzione deve stampare tutte le proprietà
+// Crea un oggetto user che rispetta il tipo UserAccount e passalo alla funzione printAccount
+
+{
+    type LoginData = {
+        username: string,
+        password: string
+    }
+
+    type Profile = {
+        name: string,
+        age: number
+    }
+
+    type UserAccount = LoginData & Profile;
+
+    function printAccount(user: UserAccount): void {
+
+        console.log(`Username: ${user.username}`);
+        console.log(`Password: ${user.password}`);
+        console.log(`Nome: ${user.name}`);
+        console.log(`Età: ${user.age}`);
+    }
+
+    const user: UserAccount = {
+        username: 'Tony70',
+        password: 'ciao',
+        name: 'Antonio',
+        age: 40
+    }
+
+    printAccount(user);
+}
