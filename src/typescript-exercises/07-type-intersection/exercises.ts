0c637f49af740f5e69e6da135aaf88abc7d5d46a
giorgiameffe <giorgiameffe03@gmail.com>
2025-07-15 13:03:09 +0200

    feat: complete exercise three (type-intersection)

--- src/typescript-exercises/07-type-intersection/exercises.ts
+++ src/typescript-exercises/07-type-intersection/exercises.ts
@@ -63,3 +63,53 @@
 
     printAccount(user);
 }
+
+// ✏️ Esercizio 3 – Controllo permessi utente
+// Crea due tipi:
+// UserInfo con le proprietà: name e email
+// Permissions con le proprietà: isAdmin canEdit
+// Crea un tipo AdminUser che sia l’intersezione tra UserInfo e Permissions.
+// Scrivi una funzione printPermissions che accetta un oggetto di tipo AdminUser e stampa:
+// "Admin: sì" se isAdmin è true
+// "Può modificare: sì" se canEdit è true
+// e i dati dell’utente (name, email)
+// Crea almeno due oggetti AdminUser diversi e chiamali nella funzione.
+
+{
+    type UserInfo = {
+        name: string,
+        email: string
+    }
+
+    type Permissions = {
+        isAdmin: boolean,
+        canEdit: boolean
+    }
+
+    type AdminUser = UserInfo & Permissions;
+
+    function printPermissions(user: AdminUser) {
+
+        console.log(`Nome: ${user.name}`);
+        console.log(`Email: ${user.email}`);
+        user.isAdmin === true ? console.log(`Admin: sì`) : console.log(`Non è un Admin`);
+        user.canEdit === true ? console.log(`Può modificare: sì`) : console.log(`Non si può modificare`);
+    }
+
+    const userOne: AdminUser = {
+        name: 'Pamela',
+        email: 'pam@ciao.it',
+        isAdmin: true,
+        canEdit: true
+    }
+
+    const userTwo: AdminUser = {
+        name: 'Luca',
+        email: 'luca@ciao.it',
+        isAdmin: false,
+        canEdit: false
+    }
+
+    printPermissions(userOne);
+    printPermissions(userTwo);
+}
